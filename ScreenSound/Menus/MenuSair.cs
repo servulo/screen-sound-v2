9c74a1a1b45e689c55cd90dd27519c7aa55811e7
Sérvulo Pires da Rocha Júnior <servulo.jr@gmail.com>
2025-01-18 14:53:36 -0300

    Finalizando a aula 3

---
+++ ScreenSound/Menus/MenuSair.cs
@@ -0,0 +1,9 @@
+﻿using ScreenSound.Menus;
+using ScreenSound.Modelos;
+internal class MenuSair : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        Console.WriteLine("Obrigado por usar o Screen Sound 2.0!");
+    }
+}
